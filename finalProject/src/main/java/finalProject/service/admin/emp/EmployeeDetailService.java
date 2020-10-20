1d3e6a80eddfc6d33a9d28956d8b1c65235ed763
babypig <seongae0226@gmail.com>
2020-10-20 10:47:55 +0900

    20201020 함성애
    관리자페이지수정

--- finalProject/src/main/java/finalProject/service/admin/emp/EmployeeDetailService.java
+++ finalProject/src/main/java/finalProject/service/admin/emp/EmployeeDetailService.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import finalProject.domain.AddressDTO;
 import finalProject.domain.EmployeeDTO;
 import finalProject.domain.StartEndPageDTO;
+import finalProject.mapper.DepMapper;
 import finalProject.mapper.EmployeeMapper;
 import finalProject.service.addr.AddressService;
 
@@ -17,12 +18,16 @@ public class EmployeeDetailService {
 	@Autowired
 	EmployeeMapper employeeMapper;
 	@Autowired
+	DepMapper depMapper;
+	@Autowired
 	AddressService addressService;
 	public void empDetail(String employeeNo, Model model) {
 		StartEndPageDTO dto = new StartEndPageDTO(1L, 1L, null, employeeNo);
 		EmployeeDTO list = employeeMapper.getEmpList(dto).get(0);
 		AddressDTO addr = addressService.devideAddress(list.getEmployeeAddr());
 		list.setEmployeeAddr(addressService.rebuildAddress(list.getEmployeeAddr()));
+		dto.setNum(list.getDepartmentNo());
+		model.addAttribute("departmentName", depMapper.getDepList(dto).get(0).getDepartmentName());
 		model.addAttribute("list", list);
 		model.addAttribute("addr", addr);
 		
